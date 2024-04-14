6e9bc3f317641755f7cfe306f7cbaf2b1351c469
itz-amethyst <pransermi@gmail.com>
2024-04-15 00:08:45 +0330

    feat

--- lib/route.ts
+++ lib/route.ts
@@ -1,5 +1,5 @@
 // Read the value of the DEBUG environment variable
-const DEBUG = process.env.DEBUG === 'true';
+const DEBUG = 'false';
 
 // Define the base URL based on the DEBUG value
 export const BASE_URL = DEBUG ? 'https://ask-me-api.vercel.app' : 'http://127.0.0.1:8000';
